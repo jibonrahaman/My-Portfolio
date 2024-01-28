647ddf801464bb3008df0e464e847c6e4ef0cd00
Jibon <manvirjibon@gmail.com>
2024-01-28 16:24:18 +0600

    navbar create

--- vite-project/src/Leyaouts/Navbar.jsx
+++ vite-project/src/Leyaouts/Navbar.jsx
@@ -2,15 +2,32 @@ import Flex from "../Common/Flex"
 import Images from "../Common/Images"
 import logo from "../assets/my-logo.png"
 function Navbar() {
-  return (
-    <section className="navbar py-[20px] px-[100px]">
-        <div>
-            <Flex>
-                <Images src={logo} alt={logo} className="w-[60px]"/>
-            </Flex>
-        </div>
-    </section>
-  )
+    return (
+        <section className="navbar py-[15px] px-[100px]">
+            <div>
+                <Flex className=" justify-between items-center">
+                    <Images src={logo} alt={logo} className="w-[70px]" />
+                    <div className=" w-[40%]">
+                        <ul className="flex  justify-between text-white items-center">
+                            <li className=" relative after:absolute after:content-[''] after:bottom-[-5px] after:left-0  after:w-0 after:h-1 after:rounded-md after:bg-white  duration-700 after:duration-700 hover:after:w-6  ">Home</li>
+
+                            <li className="relative after:absolute after:content-[''] after:bottom-[-5px] after:left-0  after:w-0 after:h-1 after:rounded-md after:bg-white  duration-700 after:duration-700 hover:after:w-6">Service</li>
+
+                            <li className="relative after:absolute after:content-[''] after:bottom-[-5px] after:left-0  after:w-0 after:h-1 after:rounded-md after:bg-white  duration-700 after:duration-700 hover:after:w-6">Portfolio</li>
+
+                            <li className="relative after:absolute after:content-[''] after:bottom-[-5px] after:left-0  after:w-0 after:h-1 after:rounded-md after:bg-white  duration-700 after:duration-700 hover:after:w-6">Blog</li>
+                          
+                            <li className="relative after:absolute after:content-[''] after:bottom-[-5px] after:left-0  after:w-0 after:h-1 after:rounded-md after:bg-white  duration-700 after:duration-700 hover:after:w-6">About</li>
+
+                            
+                        <button type="button" className="px-6 py-2 rounded-[5px]  text-base bg-first text-white border border-eight hover:bg-transparent"> Contact</button>
+                        </ul>
+
+                    </div>
+                </Flex>
+            </div>
+        </section>
+    )
 }
 
 export default Navbar
