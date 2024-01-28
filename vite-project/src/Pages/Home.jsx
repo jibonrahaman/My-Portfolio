2f135be8a3ea78133e05df33036b130f0c263db7
Jibon <manvirjibon@gmail.com>
2024-01-28 14:24:02 +0600

    home pages setup

---
+++ vite-project/src/Pages/Home.jsx
@@ -0,0 +1,10 @@
+
+function Home() {
+  return (
+    <div className=" bg-gradient-to-r from-first via-second via-third via-four via-five via-six via-seven via-eight via-nine via-ten via-eleven via-twelve to-thirteen ">
+      <h1>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod sed atque eos officiis amet qui exercitationem quaerat assumenda enim. Veniam beatae laudantium nobis aspernatur repudiandae minus, tempora ullam nulla laborum. Voluptatem tempore sequi molestias quos, neque odio ab dignissimos inventore rem nam ut, et dolorem unde nostrum asperiores nemo sed quidem reprehenderit, magni id quasi? Voluptatum voluptatem, accusantium corrupti eius porro blanditiis quasi praesentium repellendus fuga possimus a, exercitationem enim dolores, ab ipsa eaque atque molestiae iure! Blanditiis velit dolore, ex veritatis quis sit voluptate harum necessitatibus modi quia suscipit, sequi accusamus deleniti excepturi eum totam rem voluptas corporis accusantium.</h1>
+    </div>
+  )
+}
+
+export default Home
--- vite-project/tailwind.config.js
+++ vite-project/tailwind.config.js
@@ -6,16 +6,19 @@ export default {
   ],
   theme: {
     extend: {
+      maxWidth:{
+      "container":"1320px"
+      },
       colors:{
         "first" : "#141b2a",
         "second" : "#262d3c",
         "third" : "#171E2D",
         "four" : "#1E2534",
-        "five" : "#3D4452",
-        "six" : "#3D4452",
-        "seven" : "#333A49",
-        "eight" : "#282f3d",
-        "nine" : "#3d4453",
+        "five" : "#171E2D",
+        "six" : "#171E2D",
+        "seven" : "#181f2e",
+        "eight" : "#222938",
+        "nine" : "#1d2433",
         "ten" : "#3D4452",
         "eleven" : "#373e4d",
         "twelve" : "#3d4453",
