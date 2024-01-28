4123e04954c6732cd8d5026824bdae3ca6d18cba
Jibon <manvirjibon@gmail.com>
2024-01-28 13:59:54 +0600

    linear color setup

--- vite-project/tailwind.config.js
+++ vite-project/tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "first" : "#141b2a",
+        "second" : "#262d3c",
+        "third" : "#171E2D",
+        "four" : "#1E2534",
+        "five" : "#3D4452",
+        "six" : "#3D4452",
+        "seven" : "#333A49",
+        "eight" : "#282f3d",
+        "nine" : "#3d4453",
+        "ten" : "#3D4452",
+        "eleven" : "#373e4d",
+        "twelve" : "#3d4453",
+        "thirteen" : "#3d4453",
+      }
+    },
   },
   plugins: [],
 }
