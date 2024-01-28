d394e3b950c8f873a1df4752f5c01c41ccd48029
Jibon <manvirjibon@gmail.com>
2024-01-28 15:51:49 +0600

    logo

--- vite-project/src/Leyaouts/Navbar.jsx
+++ vite-project/src/Leyaouts/Navbar.jsx
@@ -1,11 +1,12 @@
 import Flex from "../Common/Flex"
-
+import Images from "../Common/Images"
+import logo from "../assets/my-logo.png"
 function Navbar() {
   return (
-    <section className="navbar">
+    <section className="navbar py-[20px] px-[100px]">
         <div>
             <Flex>
-                
+                <Images src={logo} alt={logo} className="w-[60px]"/>
             </Flex>
         </div>
     </section>
