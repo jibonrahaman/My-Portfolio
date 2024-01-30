e5ebf113f3e61439c2d6b26ed5b4e97a40a4d3ca
Jibon <manvirjibon@gmail.com>
2024-01-31 00:15:15 +0600

    design my introduction

--- src/Leyaouts/Banner.jsx
+++ src/Leyaouts/Banner.jsx
@@ -3,7 +3,11 @@ function Banner() {
   return (
    <section className="banner "> 
 <div className=" rounded-lg shadow-shadow p-8 max-w-screen-lg mx-auto ">
-  <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Exercitationem voluptate iure fugiat qui possimus nemo itaque architecto accusamus amet, neque at recusandae, aliquam et voluptatibus, officiis beatae nesciunt blanditiis. Dicta?</p>
+  <div className=" text-white text-2xl italic max-w-md text-center">
+    <h2 className="text-4xl  font-extrabold text-gray-300 ">MD Shahriar Fardous</h2>
+    <h4 className=" font-semibold"> I AM A  BACK END DEVELOPER </h4>
+    <a href="mailto:mdshahriarfardous407@gmail.com" className="hover:underline taxt-base">mdshahriarfardous407@gmail.com</a>
+  </div>
 </div>
    </section>
   )
