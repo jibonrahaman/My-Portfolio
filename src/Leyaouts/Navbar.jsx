0734e9e3afc79cf81faae2c638e567f5e5d65228
Jibon <manvirjibon@gmail.com>
2024-01-30 23:55:01 +0600

    navbar some problem fixed

--- src/Leyaouts/Navbar.jsx
+++ src/Leyaouts/Navbar.jsx
@@ -3,13 +3,13 @@ import Images from "../Common/Images"
 import logo from "../assets/my-logo.png"
 function Navbar() {
     return (
-        <section className="navbar py-[15px] px-[100px]">
+        <section className="navbar    px-[100px]">
             <div>
-                <Flex className=" justify-between items-center">
-                    <div className="w-[6%]">
+                <Flex className=" justify-between items-center ">
+                    <div className="w-[5%] pt-[10px]">
                     <Images src={logo} alt={logo}  />
                     </div>
-                    <div className=" w-[40%]">
+                    <div className=" w-[35%]">
                         <ul className="flex  justify-between text-white items-center">
                             <li className=" relative after:absolute after:content-[''] after:bottom-[-5px] after:left-0  after:w-0 after:h-1 after:rounded-md after:bg-white  duration-700 after:duration-700 hover:after:w-6  ">Home</li>
 
@@ -22,10 +22,11 @@ function Navbar() {
                             <li className="relative after:absolute after:content-[''] after:bottom-[-5px] after:left-0  after:w-0 after:h-1 after:rounded-md after:bg-white  duration-700 after:duration-700 hover:after:w-6">About</li>
 
                             
-                        <button type="button" className="px-6 py-2 rounded-[5px]  text-base bg-first text-white border border-eight hover:bg-transparent"> Contact</button>
+                
                         </ul>
 
                     </div>
+                    <button type="button" className="px-6 py-2 rounded-[5px]  text-base bg-first text-white border border-eight hover:bg-transparent"> Contact</button>
                 </Flex>
             </div>
         </section>
