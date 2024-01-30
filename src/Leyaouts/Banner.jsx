01bf5bd544b9f1d4e7edca0057377bb80085c454
Jibon <manvirjibon@gmail.com>
2024-01-31 00:34:37 +0600

    design banner part

--- src/Leyaouts/Banner.jsx
+++ src/Leyaouts/Banner.jsx
@@ -3,10 +3,11 @@ function Banner() {
   return (
    <section className="banner "> 
 <div className=" rounded-lg shadow-shadow p-8 max-w-screen-lg mx-auto ">
-  <div className=" text-white text-2xl italic max-w-md text-center">
-    <h2 className="text-4xl  font-extrabold text-gray-300 ">MD Shahriar Fardous</h2>
-    <h4 className=" font-semibold"> I AM A  BACK END DEVELOPER </h4>
+  <div className=" text-white text-2xl max-w-md  text-center">
+    <h2 className="text-4xl italic font-extrabold text-gray-300 ">MD Shahriar Fardous</h2>
+    <h4 className=" font-semibold italic my-2"> I AM A  BACK END DEVELOPER </h4>
     <a href="mailto:mdshahriarfardous407@gmail.com" className="hover:underline taxt-base">mdshahriarfardous407@gmail.com</a>
+    <p className="text-lg mt-6">By mastering Node.js and Express.js alongside HTML, CSS, JavaScript, and React.js, I craft cohesive, dynamic web solutions with unified frontend and backend capabilities. </p>
   </div>
 </div>
    </section>
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -3,7 +3,7 @@ import Navbar from "../Leyaouts/Navbar"
 
 function Home() {
   return (
-    <div className=" bg-gradient-to-br from-gray-900 to-gray-500">
+    <div className=" bg-gradient-to-br from-gray-900 to-gray-600">
       <Navbar/>
       <Banner/>
     </div>
