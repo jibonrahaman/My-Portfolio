97922862bdc347da11cc13a550c6b265c97970d9
Jibon <manvirjibon@gmail.com>
2024-02-02 23:56:42 +0600

    git & email icon linked setup

--- src/Leyaouts/Banner.jsx
+++ src/Leyaouts/Banner.jsx
@@ -13,17 +13,16 @@ function Banner() {
 
       <div className=" rounded-lg shadow-shadow p-8 max-w-[950px] mx-auto ">
         <Flex className=" justify-between gap-x-4 ">
+
           <div className=" text-white text-2xl max-w-md  text-center">
             <h2 className="text-4xl  font-extrabold">MD Shahriar Fardous</h2>
-
             <h4 className=" font-semibold italic mt-2  text-red-200 "> I AM A {' '} 
               <TypeAnimation
                 sequence={[                 
                   ' FRONT END DEVELOPER',
                   1500, 
                   'BACK END DEVELOPER',
-                  1500,
-                  
+                  1500,                  
                 ]}
                 wrapper="span"
                 speed={50}
@@ -33,22 +32,38 @@ function Banner() {
                </h4>
             <a href="mailto:mdshahriarfardous407@gmail.com" className="text-base hover:underline ">mdshahriarfardous407@gmail.com</a>
             <p className="text-lg mt-8 mb-12">By mastering Node.js and Express.js alongside HTML, CSS, JavaScript, and React.js, I craft cohesive, dynamic web solutions with unified frontend and backend capabilities. </p>
+
+            <Flex className=" justify-center gap-x-3">
+            <a href="https://github.com/jibonrahaman">
+            <div className="px-6 text-xl py-2 flex bg-black text-white items-center gap-x-2 rounded-md">
+                <FaGithub />
+              <p>Github</p>
+              </div>
+            </a>
+             
+             <a href="mailto:mdshahriarfardous407@gmail.com">
+              <div className="px-6 text-xl py-2 flex bg-black text-white items-center gap-x-2 rounded-md">
+              <MdMarkEmailRead />
+              <p>Email</p>
+              </div>
+             </a>
+
+         
+          
+          </Flex>
+
           </div>
+         
+
+
           <div className=" w-64 h-64 rounded-full flex justify-center items-center ">
             <Images src={MyPhoto} alt={MyPhoto} className="w-full h-full rounded-full" />
-          </div>
+          </div>         
         </Flex>
 
-          <Flex>            
-          <FaGithub />
-          <FaFacebook />
-          <FaInstagram />
-          </Flex> 
+        
           
-          <Flex>            
-          <FaLinkedin />
-          <MdMarkEmailRead />
-          </Flex>
+         
       
       </div>
 
