49368f75387c86c8b44727a94f1e9b649d0de0d6
Jibon <manvirjibon@gmail.com>
2024-02-03 00:30:02 +0600

    skill & summary part

---
+++ src/Leyaouts/Summary.jsx
@@ -0,0 +1,20 @@
+import Flex from "../Common/Flex"
+
+function Summary() {
+  return (
+<section className="skill-summary mx-auto max-w-container">
+<Flex className=" justify-between">
+    <div className=" w-[40%]">
+    <p>I am a passionate web application and software developer specializing in MERN Stack web development. As a frontend developer, I bring a strong foundation in HTML, CSS, and JavaScript, coupled with deep expertise in React.js. I can develop any Figma or PSD template into React.js, and it will be responsive. I develop using modern frameworks like Tailwind CSS to create visually stunning websites. Currently, I`m working with Node.js, MongoDB, and Express.js. Recently, I developed many  website, which showcases my capabilities and sets me apart from others in the field</p>
+    </div>
+    
+    <div className=" w-[40%]">
+    <p>I am a passionate web application and software developer specializing in MERN Stack web development. As a frontend developer, I bring a strong foundation in HTML, CSS, and JavaScript, coupled with deep expertise in React.js. I can develop any Figma or PSD template into React.js, and it will be responsive. I develop using modern frameworks like Tailwind CSS to create visually stunning websites. Currently, I'm working with Node.js, MongoDB, and Express.js. Recently, I developed many  website, which showcases my capabilities and sets me apart from others in the field</p>
+    </div>
+
+</Flex>
+</section>
+  )
+}
+
+export default Summary
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,11 +1,14 @@
 import Banner from "../Leyaouts/Banner"
 import Navbar from "../Leyaouts/Navbar"
+import Summary from "../Leyaouts/Summary"
+
 
 function Home() {
   return (
-    <div className=" bg-gradient-to-br from-gray-900 to-gray-600">
+    <div className=" bg-gradient-to-br from-gray-900 to-gray-600 ">
       <Navbar/>
       <Banner/>
+      <Summary/>
     </div>
   )
 }
