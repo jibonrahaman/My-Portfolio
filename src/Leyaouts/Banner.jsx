fdcb4c6a414d4cf767c678fcb3c48723d14d828a
Jibon <manvirjibon@gmail.com>
2024-02-02 23:23:35 +0600

    react icon set-up

--- src/Leyaouts/Banner.jsx
+++ src/Leyaouts/Banner.jsx
@@ -2,15 +2,21 @@ import Flex from "../Common/Flex"
 import MyPhoto from "../assets/my-photo.png"
 import Images from "../Common/Images"
 import { TypeAnimation } from 'react-type-animation';
+import { FaGithub } from "react-icons/fa";
+import { FaFacebook } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa6";
+import { FaLinkedin } from "react-icons/fa";
+import { MdMarkEmailRead } from "react-icons/md";
 function Banner() {
   return (
     <section className="banner pt-14 pb-6">
+
       <div className=" rounded-lg shadow-shadow p-8 max-w-[950px] mx-auto ">
         <Flex className=" justify-between gap-x-4 ">
           <div className=" text-white text-2xl max-w-md  text-center">
-            <h2 className="text-4xl italic font-extrabold text-gray-300 ">MD Shahriar Fardous</h2>
+            <h2 className="text-4xl  font-extrabold">MD Shahriar Fardous</h2>
 
-            <h4 className=" font-semibold italic mt-2  "> I AM A {' '} 
+            <h4 className=" font-semibold italic mt-2  text-red-200 "> I AM A {' '} 
               <TypeAnimation
                 sequence={[                 
                   ' FRONT END DEVELOPER',
@@ -32,7 +38,21 @@ function Banner() {
             <Images src={MyPhoto} alt={MyPhoto} className="w-full h-full rounded-full" />
           </div>
         </Flex>
+
+          <Flex>            
+          <FaGithub />
+          <FaFacebook />
+          <FaInstagram />
+          </Flex> 
+          
+          <Flex>            
+          <FaLinkedin />
+          <MdMarkEmailRead />
+          </Flex>
+      
       </div>
+
+   
     </section>
   )
 }
