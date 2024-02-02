6618b1d88429765d1f2c7bfe8ce10687f1ba9cb9
Jibon <manvirjibon@gmail.com>
2024-02-03 00:51:15 +0600

    summary part- summary p tag

--- src/Leyaouts/Summary.jsx
+++ src/Leyaouts/Summary.jsx
@@ -2,13 +2,16 @@ import Flex from "../Common/Flex"
 
 function Summary() {
   return (
-<section className="skill-summary mx-auto max-w-container">
+<section className="skill-summary mx-auto max-w-container md:mt-10">
 <Flex className=" justify-between">
-    <div className=" w-[40%]">
-    <p>I am a passionate web application and software developer specializing in MERN Stack web development. As a frontend developer, I bring a strong foundation in HTML, CSS, and JavaScript, coupled with deep expertise in React.js. I can develop any Figma or PSD template into React.js, and it will be responsive. I develop using modern frameworks like Tailwind CSS to create visually stunning websites. Currently, I`m working with Node.js, MongoDB, and Express.js. Recently, I developed many  website, which showcases my capabilities and sets me apart from others in the field</p>
+    <div className=" w-[40%] text-white ">
+        <h2 className=" text-3xl font-bold text-sky-400">Summary</h2>
+    <p className=" mt-5 text-[17px] w-[90%] leading-6 font-sans">I am a passionate web application and software developer specializing in MERN Stack web development. As a frontend developer, I bring a strong foundation in HTML, CSS, and JavaScript, coupled with deep expertise in React.js. I can develop any Figma or PSD template into React.js, and it will be responsive. I develop using modern frameworks like Tailwind CSS to create visually stunning websites. Currently, I`m working with Node.js, MongoDB, and Express.js. Recently, I developed many  website, which showcases my capabilities and sets me apart from others in the field</p>
     </div>
     
-    <div className=" w-[40%]">
+    <div className=" w-[40%] text-white">
+    <h2 className=" text-3xl font-bold text-sky-400">My Skills</h2>
+    
     <p>I am a passionate web application and software developer specializing in MERN Stack web development. As a frontend developer, I bring a strong foundation in HTML, CSS, and JavaScript, coupled with deep expertise in React.js. I can develop any Figma or PSD template into React.js, and it will be responsive. I develop using modern frameworks like Tailwind CSS to create visually stunning websites. Currently, I'm working with Node.js, MongoDB, and Express.js. Recently, I developed many  website, which showcases my capabilities and sets me apart from others in the field</p>
     </div>
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,21 +9,9 @@ export default {
       maxWidth:{
       "container":"1320px"
       },
-      colors:{
-        "first" : "#141b2a",
-        "second" : "#262d3c",
-        "third" : "#171E2D",
-        "four" : "#1E2534",
-        "five" : "#171E2D",
-        "six" : "#171E2D",
-        "seven" : "#181f2e",
-        "eight" : "#222938",
-        "nine" : "#1d2433",
-        "ten" : "#3D4452",
-        "eleven" : "#373e4d",
-        "twelve" : "#3d4453",
-        "thirteen" : "#3d4453",
-      },
+      fontFamily :{
+       "dm":["Roboto"]
+      },      
       boxShadow:{
         'shadow':"0 10px 15px -3px rgba(0,0,0,.3)",
         'online':"0 4px 6px -4px rgba(0,0,0,.1);",
