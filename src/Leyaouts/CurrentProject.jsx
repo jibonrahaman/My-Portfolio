8385b48c5a25f33419c5ffa8c0929881c41ef1a5
Jibon <manvirjibon@gmail.com>
2024-02-06 19:58:29 +0600

    img hover

--- src/Leyaouts/CurrentProject.jsx
+++ src/Leyaouts/CurrentProject.jsx
@@ -1,5 +1,7 @@
 import Header from "../Common/Header"
 import orebi from '../assets/orebiCurrent.png'
+import Images from '../Common/Images'
+import Flex from '../Common/Flex'
 function CurrentProject() {
     return (
         <section className="current-project mt-[110px] max-w-container mx-auto">
@@ -7,9 +9,18 @@ function CurrentProject() {
             <span className="text-sky-700"> Projects</span>
           </h2>
 
-            <div>
-      
-            </div>
+          <div className="w-full p-16">
+         <Flex  className=" justify-between">
+         <div className="w-[40%] relative group overflow-hidden">
+
+      <div className="scale-105 group">
+      <Images src={orebi} alt={orebi} className="group-hover:scale-110 duration-500"  />
+      </div>
+
+      <div className=" w-full h-full bg-black/70 absolute top-0 left-[-300px] group-hover:left-0 duration-500   "></div>
+         </div>
+         </Flex>
+          </div>
         </section>
     )
 }
