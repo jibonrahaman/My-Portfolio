e5bd755cafd72dab7fee5fd64c0a4ca9b133c99e
Jibon <manvirjibon@gmail.com>
2024-02-06 19:20:07 +0600

    header design

--- src/Leyaouts/CurrentProject.jsx
+++ src/Leyaouts/CurrentProject.jsx
@@ -1,12 +1,14 @@
 import Header from "../Common/Header"
-
+import orebi from '../assets/orebiCurrent.png'
 function CurrentProject() {
     return (
         <section className="current-project mt-[110px] max-w-container mx-auto">
-            <Header className=" text-center" text="My Current Project" />
+          <h2 className=" text-3xl font-bold text-sky-400">Latest Working 
+            <span className="text-sky-700"> Projects</span>
+          </h2>
 
             <div>
-
+      
             </div>
         </section>
     )
