679d78dbb3718cddb9019754aab52d845607fefd
Jibon <manvirjibon@gmail.com>
2024-02-06 20:21:39 +0600

    Current project photo design done

--- src/Leyaouts/CurrentProject.jsx
+++ src/Leyaouts/CurrentProject.jsx
@@ -9,7 +9,7 @@ function CurrentProject() {
     return (
         <section className="current-project mt-[110px] max-w-container mx-auto">
           <h2 className=" text-3xl font-bold text-sky-400">Latest Working 
-            <span className="text-sky-700"> Projects</span>
+            <span className="text-sky-600"> Projects</span>
           </h2>
 
           <div className="w-full p-16">
@@ -22,15 +22,18 @@ function CurrentProject() {
 
       <div className=" w-full h-full bg-black/70 absolute top-0 left-[-100%] group-hover:left-0 duration-500   "></div>
 
-      <div  className="w-full h-full  absolute top-0 left-[-100%] group-hover:left-0 duration-500  flex justify-center items-center gap-x-8 text-white text-2xl font-bold font-sans underline">
-         <Flex className=" items-center justify-center gap-x-2">
+      <div  className="w-full h-full  absolute top-0 left-[-100%] group-hover:left-0 duration-500  flex justify-center items-center gap-x-10 text-white text-lg font-bold font-sans underline">
+
+         <Flex className="  gap-x-2">
          <FaGithubAlt size={30} />
          <p >View Github</p>
          </Flex>
-         <Flex className=" items-center justify-center gap-x-2">
+
+         <Flex className="   gap-x-2">
          <SiOpenproject  size={30} />
          <p>Live Project</p>
          </Flex>
+
       </div>
          </div>
          </Flex>
