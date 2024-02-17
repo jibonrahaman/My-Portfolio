57436858645c16dce52788046296fc0d887bb74c
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-02-17 20:44:13 +0600

    done project show

--- src/Leyaouts/CurrentProject.jsx
+++ src/Leyaouts/CurrentProject.jsx
@@ -43,14 +43,16 @@ function CurrentProject() {
                 </div>
                 <div className=" w-full h-full bg-black/70 absolute top-0 left-[-100%] group-hover:left-0 duration-500   "></div>
                 <div className="w-full h-full  absolute top-0 left-[-100%] group-hover:left-0 duration-500  flex justify-center items-center gap-x-10 text-white text-lg font-bold font-sans underline">
-                  <Flex className="  gap-x-2">
-                    <FaGithubAlt size={30} />
+                  
+                   <button className="flex gap-x-2">
+                   <FaGithubAlt size={30} />
                     <p >View Github</p>
-                  </Flex>
-                  <Flex className="   gap-x-2">
-                    <SiOpenproject size={30} />
-                    <p>Live Project</p>
-                  </Flex>
+                   </button>
+                  
+                   <button className=" flex gap-x-2">                 
+                   <SiOpenproject size={30} />
+                    <p>Live Project</p>                  
+                   </button>
                 </div>
               </div>
               <div className=' w-[55%] text-white'>
@@ -81,52 +83,16 @@ function CurrentProject() {
                 </div>
                 <div className=" w-full h-full bg-black/70 absolute top-0 left-[-100%] group-hover:left-0 duration-500   "></div>
                 <div className="w-full h-full  absolute top-0 left-[-100%] group-hover:left-0 duration-500  flex justify-center items-center gap-x-10 text-white text-lg font-bold font-sans underline">
-                  <Flex className="  gap-x-2">
-                    <FaGithubAlt size={30} />
+                  
+                   <button className="flex gap-x-2">
+                   <FaGithubAlt size={30} />
                     <p >View Github</p>
-                  </Flex>
-                  <Flex className="   gap-x-2">
-                    <SiOpenproject size={30} />
-                    <p>Live Project</p>
-                  </Flex>
-                </div>
-              </div>
-              <div className=' w-[55%] text-white'>
-                <h2 className=' text-3xl font-bold'>Orebi E-Commerce</h2>
-                <p className='my-4 w-[70%] text-purple-200'>On this website, user and vendors can sign up and log in. The seller can sell the product and the user can buy the product. The website is fully responsive.</p>
-                <p className=' text-xl mt-5 '>Use is Project :</p>
-                <div className='flex gap-x-8 mt-6'>
-                  <p className='text-black px-5 py-2 bg-white rounded-md'>React</p>
-                  <p className='text-black px-5 py-2 bg-white rounded-md'>Tailwind</p>
-                  <p className='text-black px-5 py-2 bg-white rounded-md'>Redux</p>
-                  <p className='text-black px-5 py-2 bg-white rounded-md'>Javascript</p>
-                  <p className='text-black px-5 py-2 bg-white rounded-md'>Context Api</p>
-                </div>
-              </div>
-
-            </Flex>
-          </div>
-        </div>
-
-
-        <div>
-          <div className="w-full p-14">
-            <Flex className=" justify-between">
-              <div className="w-[40%] relative group overflow-hidden">
-
-                <div className="scale-105 group">
-                  <Images src={orebi} alt={orebi} className="group-hover:scale-110 duration-500" />
-                </div>
-                <div className=" w-full h-full bg-black/70 absolute top-0 left-[-100%] group-hover:left-0 duration-500   "></div>
-                <div className="w-full h-full  absolute top-0 left-[-100%] group-hover:left-0 duration-500  flex justify-center items-center gap-x-10 text-white text-lg font-bold font-sans underline">
-                  <Flex className="  gap-x-2">
-                    <FaGithubAlt size={30} />
-                    <p >View Github</p>
-                  </Flex>
-                  <Flex className="   gap-x-2">
-                    <SiOpenproject size={30} />
-                    <p>Live Project</p>
-                  </Flex>
+                   </button>
+                  
+                   <button className=" flex gap-x-2">                 
+                   <SiOpenproject size={30} />
+                    <p>Live Project</p>                  
+                   </button>
                 </div>
               </div>
               <div className=' w-[55%] text-white'>
@@ -146,6 +112,9 @@ function CurrentProject() {
 
           </div>
         </div>
+
+
+        
 
       </Slider>
 
