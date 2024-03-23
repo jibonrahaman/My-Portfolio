2cb0bb3392a1e9bf494a88181e1d9227b97cf657
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-03-23 16:54:30 +0600

    portfolio layout done! setup protfolioData ,mixitup category and filter data and map all data & all setup done

--- src/Data/portfolio.js
+++ src/Data/portfolio.js
@@ -1,97 +1,112 @@
 let portfolio =[
     {
-        title : "avada",
+        title : "chitchat",
         url:"./images/a.png",
-        type : ["Html Css","Bootstrap"]
-    },
-    {
-        title : "discover",
-        url:"./images/b.png",
-        type : ["Html Css","Taillwind Css"]
-    },
-    {
-        title : "omah",
-        url:"./images/c.png",
-        type : ["Taillwind Css"]
-    },
-    {
-        title : "creative",
-        url:"./images/d.png",
-        type : ["Html Css","Bootstrap","Javascript"] 
-    },
-    {
-        title : "innovate",
-        url:"./images/e.png",
         type : ["Taillwind Css","Javascript","React Js"]
     },
     {
-        title : "charger",
-        url:"./images/f.png",
-        type : ["Html Css","Javascript"]
-       
+        title : "orebi",
+        url:"./images/b.png",
+        type : ["Taillwind Css","Javascript","React Js"]
     },
     {
-        title : "daraz",
-        url:"./images/g.png",
-        type : ["Html Css","Taillwind Css","Javascript","React Js"]
-    },
-    {
-        title : "Orabai",
-        url:"./images/z.png",
-        type : ["Html Css","Taillwind Css","Javascript","React Js"]
-    },
-    {
-        title : "tonix",
-        url:"./images/t1.png",
-        type : ["Html Css","Taillwind Css","Javascript","React Js"]
-    },
-    {
-        title : "eduford",
-        url:"./images/h.png",
-        type : ["Html Css","Bootstrap"]
-        
-    },
-    {
-        title : "waxom",
-        url:"./images/w.png",
-        type : ["Bootstrap"]
-       
-    },
-    {
-        title : "you tube",
-        url:"./images/j.png",
-        type : ["Bootstrap"]
-       
-    },
-    {
-        title : "sequrity",
-        url:"./images/k.png",
+        title : "registration",
+        url:"./images/c.png",
         type : ["Taillwind Css","Javascript","React Js"]
     },
     {
         title : "todo",
-        url:"./images/l.png",
-        type : ["Javascript","React Js","Game"]
+        url:"./images/d.png",
+        type : ["Html Css","Javascript"] 
+    },
+    {
+        title : "todos",
+        url:"./images/e.png",
+        type : ["Html Css","Javascript"]
+    },
+    {
+        title : "instagram",
+        url:"./images/f.png",
+        type : ["Taillwind Css","React Js"]
+       
+    },
+    {
+        title : "slider",
+        url:"./images/g.png",
+        type : ["Html Css","Javascript"]
     },
     {
         title : "game",
+        url:"./images/h.png",
+        type : ["Html Css","Javascript"]
+    },
+    {
+        title : "faq",
+        url:"./images/i.png",
+        type : ["Html Css","Javascript"]
+    },
+    {
+        title : "mixitup",
+        url:"./images/j.png",
+        type : ["Html Css","Javascript"]
+        
+    },
+    {
+        title : "me",
+        url:"./images/k.png",
+        type : ["Taillwind Css","React Js"]
+       
+    },
+    {
+        title : "omah",
+        url:"./images/l.png",
+        type : ["Html Css","Bootstrap"]
+       
+    },
+    {
+        title : "quriarbox",
         url:"./images/m.png",
-        type : ["Taillwind Css","Javascript","React Js","Game"]
-
+        type : ["Html Css", "Bootstrap"]
     },
- 
     {
-        title : "table search",
+        title : "petroil",
+        url:"./images/n.png",
+        type : ["Tailwind Css", "React Js"]
+    },
+    {
+        title : "innovate",
         url:"./images/o.png",
-        type : ["Taillwind Css","Javascript","React Js"]
+        type : ["Html Css", "Bootstrap"]
     },
- 
     {
-        title : "gameFirebase",
-        url:"./images/g3.png",
-        type : ["Taillwind Css","Game","React Js"]
+        title : "e-school",
+        url:"./images/p.png",
+        type : ["Html Css", "Bootstrap"]
+    }, {
+        title : "qwery",
+        url:"./images/q.png",
+        type : ["Html Css"]
     },
-
+    {
+        title : "finsweet",
+        url:"./images/u.png",
+        type : ["Html Css", "Bootstrap"]
+    }, 
+    {
+        title : "pet",
+        url:"./images/v.png",
+        type : ["Html Css", "Bootstrap"]
+    },  
+    {
+        title : "rabbe",
+        url:"./images/w.png",
+        type : ["Html Css", "Bootstrap"]
+    },   
+    {
+        title : "insta-demo",
+        url:"./images/x.png",
+        type : ["Tailwind Css", "React Js"]
+    }, 
 ]
 
 export default portfolio
--- src/Leyaouts/Portfolio.jsx
+++ src/Leyaouts/Portfolio.jsx
@@ -1,18 +1,67 @@
 import Header from "../Common/Header";
-
+import Images from "../Common/Images";
+import Flex from "../Common/Flex";
+import orebiCurrent from "../assets/orebiCurrent.png"
+import portfolioData from "../Data/portfolio";
+import { useState } from "react";
+import { Link } from "react-router-dom";
 export default function Portfolio() {
+  const [data,setdata] =useState (portfolioData);
+  const [portfolio,setportfolio] =useState (portfolioData);
+ const handleData = (name) =>{
+  let arr = []
+  portfolioData.filter((item)=>{
+    item.type.filter((item2) =>{
+          if(item2 == name){
+            arr.push(item)
+          }
+    })
+  })
+  setportfolio(arr)
+ }
+ console.log(portfolio);
   return (
    <section className="max-w-container mx-auto py-20">
    <Header  text="My Portfolio"/>
    <ul className=" flex justify-center gap-x-10 text-white font-bold">
-   <li className='bg-black hover:text-black hover:bg-white px-2 md:px-3 cursor-pointer py-1 duration-300 rounded-[5px]'>Html Css</li>
-   <li className='bg-black hover:text-black hover:bg-white px-2 md:px-3 cursor-pointer py-1 duration-300 rounded-[5px]'>Bootstrap</li>
-   <li className='bg-black hover:text-black hover:bg-white px-2 md:px-3 cursor-pointer py-1 duration-300 rounded-[5px]'>Taillwind Css</li>
-   <li className='bg-black hover:text-black hover:bg-white px-2 md:px-3 cursor-pointer py-1 duration-300 rounded-[5px]'>Javascript</li>
-   <li className='bg-black hover:text-black hover:bg-white px-2 md:px-3 cursor-pointer py-1 duration-300 rounded-[5px]'>React Js</li>
-   <li className='bg-black hover:text-black hover:bg-white px-2 md:px-3 cursor-pointer py-1 duration-300 rounded-[5px]'>Next Js</li>  
-   <li className='bg-black hover:text-black hover:bg-white px-2 md:px-3 cursor-pointer py-1 duration-300 rounded-[5px]'>All</li>
+   <li onClick={()=>handleData("Html Css")} className='bg-black hover:text-black hover:bg-white px-2 md:px-3 cursor-pointer py-1 duration-300 rounded-[5px]'>Html Css</li>
+   <li onClick={()=>handleData("Bootstrap")} className='bg-black hover:text-black hover:bg-white px-2 md:px-3 cursor-pointer py-1 duration-300 rounded-[5px]'>Bootstrap</li>
+   <li onClick={()=>handleData("Taillwind Css")} className='bg-black hover:text-black hover:bg-white px-2 md:px-3 cursor-pointer py-1 duration-300 rounded-[5px]'>Taillwind Css</li>
+   <li onClick={()=>handleData("Javascript")} className='bg-black hover:text-black hover:bg-white px-2 md:px-3 cursor-pointer py-1 duration-300 rounded-[5px]'>Javascript</li>
+   <li onClick={()=>handleData("React Js")} className='bg-black hover:text-black hover:bg-white px-2 md:px-3 cursor-pointer py-1 duration-300 rounded-[5px]'>React Js</li>
+   <li onClick={()=>handleData("Next Js")} className='bg-black hover:text-black hover:bg-white px-2 md:px-3 cursor-pointer py-1 duration-300 rounded-[5px]'>Next Js</li>  
+   <li onClick={()=>handleData("")} className='bg-black hover:text-black hover:bg-white px-2 md:px-3 cursor-pointer py-1 duration-300 rounded-[5px]'>All</li>
    </ul>
+   
+  <Flex className="h-[450px]  flex-wrap justify-center gap-10 mt-10 scroll overflow-y-scroll">
+{
+  portfolio.length ?  
+ (
+  portfolio.map((item,index)=>{
+    const {title,url}=item
+    return  <div key={index}  className="w-[350px] group relative overflow-hidden rounded-lg">
+   <Link to={title == "chitchat" ? "https://mern-2205-chitchat-chatting-app.vercel.app/" : title == "orebi" ? "https://orebi-fronted-e-commerce.vercel.app/" : title == "registration" ? "https://redux-using-reg-login-page.vercel.app/" : title == "todo" ? "https://todo-javascript-three.vercel.app/" : title == "todos" ?  "https://todo-class-29.vercel.app/" : title == "instagram" ? "https://instagram-clone-lemon-two.vercel.app/" : title == "slider" ? "https://class-26-img-slider.vercel.app/" : title == "game" ? "https://class-28-guess-game.vercel.app/" : title == "faq" ? "https://faq-topaz.vercel.app/" : title == "mixitup" ? "https://mixitup-simple.vercel.app/" : title == "me" ? "https://me-bay-eight.vercel.app/#Abou" : title == "omah" ? "https://omah-responsive.vercel.app/" : title == "quriarbox" ? "https://quriarbox-beige.vercel.app" : title == "petroil" ? "https://class-32-tailwind-1st-project.vercel.app" : title == "innovate" ? "https://innovet-react.vercel.app/" : title == "e-school" ? "https://e-school-iota.vercel.app/" : title == "qwery" ? "https://overview-html-css.vercel.app/" : title == "finsweet" ? "https://finswet-bootsstrap-j49h.vercel.app/" : title== "pet" ? "https://pet-html-x35t.vercel.app/" : title == "rabbe" ? "https://rabby-poushi.vercel.app/" : title == "insta-demo" ? "https://todo-about-chat-m7in.vercel.app/" : "/" }>  
+    <Images src={url} alt={url} className=" group-hover:scale-105 duration-500 rounded-xl"/>  
+     </Link>
+    <div className="w-full h-full absolute top-0 left-0 bg-black/25 group-hover:top-[-500px] "></div>
+     </div> 
+   
+  })
+ )
+  :
+ (
+  data.map((item,index)=>{
+    const {title,url}=item
+    return  <div key={index}  className="w-[350px] group relative overflow-hidden rounded-lg">
+    <Link to={title == "chitchat" ? "https://mern-2205-chitchat-chatting-app.vercel.app/" : title == "orebi" ? "https://orebi-fronted-e-commerce.vercel.app/" : title == "registration" ? "https://redux-using-reg-login-page.vercel.app/" : title == "todo" ? "https://todo-javascript-three.vercel.app/" : title == "todos" ?  "https://todo-class-29.vercel.app/" : title == "instagram" ? "https://instagram-clone-lemon-two.vercel.app/" : title == "slider" ? "https://class-26-img-slider.vercel.app/" : title == "game" ? "https://class-28-guess-game.vercel.app/" : title == "faq" ? "https://faq-topaz.vercel.app/" : title == "mixitup" ? "https://mixitup-simple.vercel.app/" : title == "me" ? "https://me-bay-eight.vercel.app/#Abou" : title == "omah" ? "https://omah-responsive.vercel.app/" : title == "quriarbox" ? "https://quriarbox-beige.vercel.app" : title == "petroil" ? "https://class-32-tailwind-1st-project.vercel.app" : title == "innovate" ? "https://innovet-react.vercel.app/" : title == "e-school" ? "https://e-school-iota.vercel.app/" : title == "qwery" ? "https://overview-html-css.vercel.app/" : title == "finsweet" ? "https://finswet-bootsstrap-j49h.vercel.app/" : title== "pet" ? "https://pet-html-x35t.vercel.app/" : title == "rabbe" ? "https://rabby-poushi.vercel.app/" : title == "insta-demo" ? "https://todo-about-chat-m7in.vercel.app/" : "/" }>  
+     <Images src={url} alt={url} className=" group-hover:scale-105 duration-500 rounded-xl"/>  
+      </Link>
+     <div className="w-full h-full absolute top-0 left-0 bg-black/25 group-hover:top-[-500px] "></div>
+      </div> 
+  })
+ )
+}
+  </Flex>
    </section>
   )
 }
