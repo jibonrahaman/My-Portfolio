45895f3650424442519342deb3492596be2e16cc
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-03-28 17:28:56 +0600

    About  layout done!

--- src/Leyaouts/About.jsx
+++ src/Leyaouts/About.jsx
@@ -1,8 +1,8 @@
-
 import { useEffect, useState } from 'react';
 import Flex from '../Common/Flex';
 import Images from '../Common/Images';
-import MyPhoto from '../assets/my-photo.png'
+import MyPhoto from '../assets/my-photo.png';
+
 export default function About() {
   const Data = [
     { demo :"Skils",
@@ -41,47 +41,44 @@ export default function About() {
   
   const skillsData = Data.filter(item => item.demo === "Skils");
   const [datas, setDatas] = useState(skillsData);
+  const [activeButton, setActiveButton] = useState("");
 
   useEffect(() => {
-    // Set default data to Skils when the component mounts
+    // Set default data to Skills when the component mounts
     setDatas(skillsData);
   }, []);
 
-const handleClick =(name)=>{
-const filt = Data.filter(item => item.demo === name);
-setDatas(filt)
-}
-
+  const handleClick = (name) => {
+    const filt = Data.filter(item => item.demo === name);
+    setDatas(filt);
+    setActiveButton(name);
+  }
 
   return (
-<section className="mx-w-container mx-auto pt-24">
-  <Flex className=" justify-center gap-x-20 ">
-   <Images src={MyPhoto} alt={MyPhoto} className="w-[25%]"/>
-   <div className='w-[40%]'>
-   <div  className=' text-gray-300 flex flex-col gap-y-3 '>
-    <h4 className=' font-bold text-4xl text-white'>About Me</h4>
-    <h3 className='font-semibold text-2xl'>A dedicated Full Stack Web Developer based in Market Place</h3>
-   <p>Assala Mualaikum! My name is Md Shahriar Fardous. I'm a Full Stack web developer specializing in ReactJs,Next Js, JavaScript, TypeScript, Node.js, Angular, and MongoDB. My focus is on creating responsive web pages and dynamic websites. Currently, I have professional experience in React Js & Next Js</p>
-   </div>
-   <Flex className="">
-   <div className=' pt-2 text-white'>
-   <button onClick={()=>handleClick("Skils")} className='font-bold text-xl shadow px-4 shadow-lime-400 hover:text-gray-400'>Skils</button> 
-   <button onClick={()=>handleClick("Experience")} className='font-bold text-xl shadow px-4 shadow-lime-400 hover:text-gray-400'>Experience</button>
-    <button onClick={()=>handleClick("Education")} className='font-bold text-xl shadow px-4 shadow-lime-400 hover:text-gray-400'>Education</button>
-    </div>       
-      </Flex>
-      {
-    datas.map ((item,index)=>{
-      return <div key={index} className=' text-white pt-4'>
-      <h5 className=' text-xl text-gray-400'> {item.title}</h5>
-      <p>{item.content}</p>        
-      <p>{item.institution}</p>        
-       </div>
-    })
-   }
-   </div>
-  </Flex> 
-  
-</section>
+    <section className="mx-w-container mx-auto pt-24">
+      <Flex className="justify-center gap-x-20">
+        <Images src={MyPhoto} alt={MyPhoto} className="w-[25%]"/>
+        <div className='w-[40%]'>
+          <div className='text-gray-300 flex flex-col gap-y-3'>
+            <h4 className='font-bold text-4xl text-white'>About Me</h4>
+            <p>I'm passionate about front-end development and aspire to build a rewarding career in this field. With proficiency in HTML, CSS, Bootstrap, Tailwind, and intermediate-level skills in JavaScript and React.js, my goal is to create user-centric web applications and websites that combine functionality with aesthetic appeal.</p>
+          </div>
+          <Flex className="pt-4 text-white gap-x-10">   
+            <button onClick={() => handleClick("Skils")} className={`font-bold text-xl shadow px-4 ${activeButton === "Skils" && "shadow-lime-400"} hover:text-gray-400`}>Skills</button> 
+            <button onClick={() => handleClick("Experience")} className={`font-bold text-xl shadow px-4 ${activeButton === "Experience" && "shadow-lime-400"} hover:text-gray-400`}>Experience</button>
+            <button onClick={() => handleClick("Education")} className={`font-bold text-xl shadow px-4 ${activeButton === "Education" && "shadow-lime-400"} hover:text-gray-400`}>Education</button>
+          </Flex>
+          {
+            datas.map((item, index) => (
+              <div key={index} className='text-white pt-4'>
+                <h5 className='text-xl text-gray-400'>{item.title}</h5>
+                <p>{item.content}</p>        
+                <p>{item.institution}</p>        
+              </div>
+            ))
+          }
+        </div>
+      </Flex> 
+    </section>
   )
 }
