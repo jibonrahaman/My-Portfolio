c85e09b82383d7c4320af449c417e070706b26d9
Md Shahriar Fardous <manvirjibon@gmail.com>
2024-05-13 12:02:28 +0600

    Problem_Solving Layout solved problem issue,img width

--- src/Leyaouts/Problem_Solving.jsx
+++ src/Leyaouts/Problem_Solving.jsx
@@ -7,8 +7,10 @@ export default function Problem_Solving() {
         <section className=" max-w-container mx-auto">
             <Header text="What sets me apart" className=" text-center" />
             <Flex className="pt-20 justify-center items-center gap-x-24">
-                <Images src={mern} alt={mern} className=" w-[28%]"/>
-                <div className='w-[40%] text-gray-300 flex flex-col gap-y-6'>
+                <div className="w-[35%]">
+                <Images src={mern} alt={mern} />
+                </div>
+                <div className='w-[40%] text-gray-300 flex flex-col gap-y-7'>
                     <h4 className=' text-white font-semibold text-2xl '>Problem-Solving Prowess, innovative Solutions and Collaborative Mindset</h4>
                     <p>I thrive on finding creative and innovative solutions to complex problems, pushing the boundaries of what's possible in web development.</p>
                     <p>tackling complex challenges and finding elegant solutions, turning obstacles into opportunities.</p>
